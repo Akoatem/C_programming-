6410648e143b85c84b89a26705782050a00d8b58
Akoatem <akoatemsam2003@gmail.com>
2022-09-23 12:05:33 +0200

    Sum of two number using pointer updated

--- additionPointer/add.c
+++ additionPointer/add.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
-
 void add(int *a, int *b, int *sum);
-void main()
+
+/**
+ * main - addition of two interger
+ * Description: This is a pointer function
+ *
+ * Return: Always 0 (success)
+ */
+
+void main(void)
 {
-    int a, b;
-    int sum = 0;
+	int a, b;
+	int sum = 0;
 
-    printf("Enter two number: ");
-    scanf("%d%d", &a, &b);
-
-    add(&a, &b, &sum);
-    printf("Sun is %d\n", sum);
-
+	printf("Enter two number: ");
+	scanf("%d%d", &a, &b);
+	add(&a, &b, &sum);
+	printf("The sum is %d\n", sum);
 }
+/**
+ * add - Addition of two numbers
+ * @a: input integer a
+ * @b: input integer b
+ * @sum: sum of the integers
+ */
 
 void add(int *a, int *b, int *sum)
 {
-    *sum = *a + *b;
+	*sum = *a + *b;
 }
