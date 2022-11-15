2ae0223076f1679371e2444eb61c2facbe0f00ac
Akoatem <akoatemsam2003@gmail.com>
2022-11-15 17:56:35 +0200

    write a shell program using execve function

--- write_your_own_shell_programs/exec.c
+++ write_your_own_shell_programs/exec.c
@@ -1,35 +1,20 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <unistd.h>
 
 /**
- * main - write in the main function
- * Description: write a program that create ls -l in shell bash
+ * main - execve example
  *
- * Return: 0 on success and -1 if execve fails
+ * Return: Always 0.
  */
 int main(void)
 {
-	pid_t pid;
-	char *argv[] = {"/bin/ls", "-l", NULL}; /* -l is to print in long format, NULL pointer is for end of string */
+    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 
-	pid = fork();
-	if (pid == -1)
-		return (-1);
-	/* check for child process */
-	if (pid == 0)
-	{
-		/* check the return value of exec function call */
-		int val = execve(argv[0], argv, NULL);
-		if (val == -1)
-		perror("Error");
-	}
-	/* check for parent using the wait function */
-	else
-	{
-		wait(NULL);
-		printf("Done with execve\n");
-	}
-	return (0);
+    printf("Before execve\n");
+    if (execve(argv[0], argv, NULL) == -1)
+    {
+        perror("Error:");
+    }
+    printf("After execve\n");
+    return (0);
 }
