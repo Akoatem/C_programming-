6216c4df01c574a738ec6d68014350a53145c4aa
Akoatem <akoatemsam2003@gmail.com>
2022-12-18 12:01:39 +0200

    display element in staack using linklist

---
+++ linklist_stack/display.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display()
+{
+	int i;
+	struct node *pointer;
+	pointer = temp;
+	if(pointer == NULL)
+	{
+		printf("Stack underflow\n");
+	}
+	else
+	{
+		printf("The elements of the stack are:\n");
+		while(pointer!= NULL)
+		{
+			printf("%d\n",pointer -> data);
+			pointer = pointer -> next;
+		}
+	}
+}
+
