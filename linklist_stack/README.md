9e50ea9d8d8e3000c0bb8b9f8611a4347ca5ba43
Akoatem <akoatemsam2003@gmail.com>
2022-12-18 11:45:38 +0200

    Add element into a stack using linklist

---
+++ linklist_stack/README.md
@@ -0,0 +1,2 @@
+The implememtation of stack using linklist in dynamic
+memory allocation(malloc) in stack overflow
---
+++ linklist_stack/insertion.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  linked lists support the dynamic memory allocation of the data elements of the stack.
+ *  Therefore, the size of the stack is allocated during the program run and needn’t be specified beforehand.
+ */
+
+void push ()
+{
+	int data;
+	struct node *pointer = (struct node*)malloc(sizeof(struct node));
+	if(pointer == NULL)
+	{
+		printf("Stack overflow");
+	}
+	else
+	{
+		printf("Enter the element to be inserted: ");
+		scanf("%d",&data);
+		if(temp == NULL)
+		{
+			pointer -> data = data;
+			pointer -> next = NULL;
+			temp = pointer;
+		}
+		else
+		{
+			pointer -> data = data;
+			pointer -> next = temp;
+			temp = pointer;
+		}
+	}
+}
