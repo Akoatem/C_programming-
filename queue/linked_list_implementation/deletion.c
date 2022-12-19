2aeda1eda224b2103c79f0f48cb1c29cc4b268b2
Akoatem <akoatemsam2003@gmail.com>
2022-12-19 10:38:32 +0200

    deletion in queue using linkedlist

---
+++ queue/linked_list_implementation/deletion.c
@@ -0,0 +1,16 @@
+void delet()
+{
+	struct node *temp;
+	temp = front;
+	if (front == NULL)
+	{
+		printf("Queue underflow\n");
+		front = rear = NULL;
+	}
+	else
+	{
+		printf("The deleted element from the queue is: %d\n", front->data);
+		front = front->link;
+		free(temp);
+	}
+}
